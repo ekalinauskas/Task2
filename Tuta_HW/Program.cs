d786bc004e230b654dce74327e540a43efe79ab6
ekalinauskas <ekalinauskas@gmail.com>
2022-04-25 21:44:37 +0300

    My first commit to task2

---
+++ Tuta_HW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tuta_HW
+{
+    class First
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter first number:");
+            string inputA = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            string inputB = Console.ReadLine();
+
+            int suma = int.Parse(inputA) + int.Parse(inputB);
+            Console.WriteLine($"Suma: {suma}");
+            Console.ReadLine();
+        }
+    }
+}
